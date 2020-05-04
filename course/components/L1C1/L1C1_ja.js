de37d34071a2a3e033e910cd33023d5b2fd8f372
hidenori ohnishi <hidenori.ohnishi@gmail.com>
2020-05-04 16:44:35 +0900

    japanese translation lesson 1 done

--- course/components/L1C1/L1C1_ja.js
+++ course/components/L1C1/L1C1_ja.js
@@ -1,13 +1,13 @@
 const local = {
-    instruction: "シリアル番号がどう城に影響を与えるか？",
+    instruction: "シリアルナンバーで城はどう変わる？",
     races : ["人間", "エルフ", "オーク", "ノーム", "アンデッド"],
-    scale : ["少し", "中", "大"],
-    serialNumber: "番号",
-    specialid: "特別な標識",
+    scale : ["小", "中", "大"],
+    serialNumber: "シリアルナンバー",
+    specialid: "スペシャルID",
     race: "種族",
-    economic: "経済",
-    military: "軍事",
-    cards: ["大きさ", "様式", "色", "ロゴ様式", "ロゴ色", "ロゴ位置", "種族", "特別な標識"],
+    economic: "経済力",
+    military: "軍事力",
+    cards: ["大きさ", "スタイル", "色", "ロゴ", "ロゴ色", "ロゴ位置", "種族", "スペシャルID"],
 };
 
 export {local};
